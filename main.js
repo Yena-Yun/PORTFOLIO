7652c4de63dbde2ec39818871c5d9bcf29857a45
Yena-Yun <cheryleduart@gmail.com>
2021-09-17 17:44:27 +0900

    포트폴리오 세부사항 작업중

---
+++ main.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// navbar가 맨 위에 있을 때는 투명하게 만들기
+const navbar = document.querySelector('#navbar');
+
+const navbarHeight = navbar.getBoundingClientRect().height;
+
+document.addEventListener('scroll', () => {
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add('navbar--dark');
+  } else {
+    navbar.classList.remove('navbar--dark');
+  }
+});
+
+// navbar 메뉴 클릭 시 해당 section으로 이동하기
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (e) => {
+  const target = e.target;
+  const link = target.dataset.link;
+  if (link === null) {
+    return;
+  }
+  console.log(link);
+});
